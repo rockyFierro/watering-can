7a348bdc4279a7bc92b23b02d055f56828b5a41a
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-04-23 23:48:55 -0400

    added axios post, login can be done through navbar if we want to design a header for it

--- watering-can-frontend/src/Signup.js
+++ watering-can-frontend/src/Signup.js
@@ -8,6 +8,8 @@ const initialValues = {
   name: '',
   email:'',
   phone:'',
+  //phone:[0], instead of a string it can be a number
+  password:'',
 };
 
 
@@ -15,6 +17,7 @@ const valueErrors = {
   name: '',
   email:'',
   phone:'',
+  password:'',
 };
 
 //state and schema stuff
@@ -24,36 +27,89 @@ const formSchema = yup.object().shape({
   name: yup.string().required('Please enter your name').min(5, 'Name must be at least 5 characters long'),
   email: yup.string().required('Must be a valid email'),
   phone: yup.string().required('Must be a valid phone number'),
+  password: yup.string().required('Please enter a valid password').min(6, 'Password must be atleast 6 characters long'),
 });
 
 
-const [signUpValues, setSignUpValues]=useState(initialValues)
-const [signUpErrors, setOrderErrors]=useState(valueErrors);
+const [signUpValues, setSignUpValues]=useState(initialValues);
+const [signUpErrors, setSignUpErrors]=useState(valueErrors);
+const [newUser, setNewUser]=useState(initialValues);
+
+const postNewUser = (newUser) => {
+  axios
+  .post('https://water-my-plants-node.herokuapp.com/api/auth/register', newUser)
+  .then((res) => {
+  setNewUser(newUser)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+const onSubmit = (e) => {
+  e.preventDefault();
+ postNewUser(signUpValues)
 
 
 
+ const inputChange = (name, value) =>{
+  yup.reach(formSchema, name)
+      .validate(value)
+      .then(() => {
+  setSignUpErrors({
+          ...signUpErrors,
+          [name]: "",
+})})
+        .catch((err) => {
+  setSignUpErrors({
+            ...signUpErrors,
+            [name]: err.errors[0],
+})});
+  setSignUpValues({
+      ...signUpValues,
+      [name]: value, 
+});
 
+const onChange = (e) => {
+  const { name, value, type, checked } = e.target;
+  const valueToUse = type === "checkbox" ? checked : value;
+  inputChange(name, valueToUse);
+};};
+
+};
 
 
 
 
 
 //main form area 
-<form action="" >
+return (<form onSubmit={onSubmit}>
   <div class="container">
     <h1>Sign Up</h1>
     <p>Please fill in to create an account.</p>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required/>
+    <label for="email"><b>{signUpValues.email}</b></label>
+    
+    <input type="text" 
+    placeholder="Enter Email" 
+    name="email" required
+    />
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
+    <label for="psw"><b>{signUpValues.password}</b></label>
+    
+    <input type="password"
+     placeholder="Enter Password" 
+    name="psw" required/>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
+    <label for="psw-repeat"><b>{signUpValues.password}</b></label>
+    
+    <input 
+    
+    type="password" 
+    placeholder="Repeat Password" 
+    name="psw-repeat" required/>
 
-<label for="phone-num"><b>Phone Number</b></label>
+<label for="phone-num"><b>{signUpValues.phone}</b></label>
+
 <input type="tel" 
 id="phone" 
 pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
@@ -71,11 +127,11 @@ pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
       <button type="button" 
       class="cancelbtn">Cancel</button>
       
-      <button type="submit" 
+      <button onClick={(evt)=> evt.preventDefault} type="submit" 
       class="signupbtn">Sign Up</button>
     </div>
   </div>
-</form>
+</form>)
 
 
 //const FormStyle = styled.div ``
